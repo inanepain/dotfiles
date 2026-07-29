a4cac20b27ea2e39add1d358021552f1523fa368
Philip Michael Raab <philip@cathedral.co.za>
2026-07-29 06:55:30 +0200

    zsh: conditionally add starship plugin for non-JetBrains terminals

--- home/zsh.d/blackbetty.zsh
+++ home/zsh.d/blackbetty.zsh
@@ -3,4 +3,8 @@
 ##################################################
 # export STARSHIP_CONFIG=~/.config/starship/develop.toml
 
-plugins+=($plugins autoupdate brew composer copybuffer dash direnv extract eza forgit iterm2 jj macos mosh rsync starship thefuck tldr tt zsh-autosuggestions zsh-interactive-cd zsh-navigation-tools)
+plugins+=($plugins autoupdate brew composer copybuffer dash direnv extract eza forgit iterm2 jj macos mosh rsync thefuck tldr tt zsh-autosuggestions zsh-interactive-cd zsh-navigation-tools)
+
+if [[ ! $TERMINAL_EMULATOR = "JetBrains-JediTerm" ]]; then
+	plugins+=($plugins starship)
+fi
