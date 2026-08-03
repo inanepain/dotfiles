7357f848cba1d4f2f64657bf03449490ad09120a
Philip Michael Raab <philip@cathedral.co.za>
2026-08-03 02:24:39 +0200

    feat: enhance logging and messaging functions; add cleanup for .DS_Store and code runner temp files

--- home/config/direnv/lib/logging.sh
+++ home/config/direnv/lib/logging.sh
@@ -1,7 +1,30 @@
 ##############################################
-#### direnv: logging
-##############################################
+#### direnv: logging & messaging
+####                               2026 Aug 03
+##################################-###########
 
+#### MSG & COLOUR
+##############################################
+## NO COLOUR
+NC='\033[0m'              # Text Reset
+
+## Regular Colors (0)
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# message with colour support
+function msg {
+    echo -e "${@}${NC}"
+}
+
+#### DEPRECATED MESSAGING
+##############################################
 function dmsg() {
     ${ENV_DIR_QUIET-false} || echo "$@"
     # if [[ ! ${ENV_DIR_QUIET-false} == true ]]; then
--- home/config/direnv/lib/utility-file.sh
+++ home/config/direnv/lib/utility-file.sh
@@ -1,5 +1,9 @@
 ##############################################
 #### direnv: utilities for files
+####                               2026 Aug 03
+##################################-###########
+
+#### .DS_Store Deprecated
 ##############################################
 # count DS_Store files
 function clean-files-count() {
@@ -9,7 +13,8 @@ function clean-files-count() {
 # Ask/Auto clean found DS_Store files
 function clean-files-osx() {
     if [[ ! $(clean-files-count) == "0" ]]; then
-		local dirtyFiles="$(clean-files-count)"
+		local dirtyFiles;
+		dirtyFiles="$(clean-files-count)"
         if [[ ${ENV_DIR_AUTO_CLEAN:-false} == true ]]; then
             delog "Found $dirtyFiles DS_Store files: AUTO CLEANING"
             find "${@:-.}" -type f -name .DS_Store -delete
@@ -20,4 +25,52 @@ function clean-files-osx() {
             fi
         fi
     fi
+}
+
+#### .DS_Store File Clean up
+##############################################
+# the count of .DS_Store files
+function files-ds-store-count() {
+	fd -IH --glob ".DS_Store" | wc -l | awk '{print $1}'
+}
+
+# echo .DS_Store count with optional count override (and message)
+function files-ds-store-echo() {
+	local dsFiles;
+	dsFiles="${*:-$(files-ds-store-count)}"
+	msg ".DS_Store Files: $Red$dsFiles"
+}
+
+# clean .DS_Store files with message
+function files-ds-store-clean() {
+	local dsFiles;
+	dsFiles="$(files-ds-store-count)"
+	if [[ ! $dsFiles == "0" ]]; then
+		fd -IH --glob ".DS_Store" "${@:-.}" -x trash
+		files-ds-store-echo "$dsFiles (CLEANED)"
+	fi
+}
+
+#### Code Runner Temp File Clean up
+##############################################
+# the count of code runner temp files
+function files-code-runner-count() {
+	fd -I --glob "tempCodeRunner*.*" | wc -l | awk '{print $1}'
+}
+
+# echo code runner count with optional count override (and message)
+function files-code-runner-echo() {
+	local crTempFiles;
+	crTempFiles="${*:-$(files-code-runner-count)}"
+	msg "Code Runner Temp Files: $Red$crTempFiles"
+}
+
+# clean code runner temp files with message
+function files-code-runner-clean() {
+	local crTempFiles;
+	crTempFiles="$(files-code-runner-count)"
+	if [[ ! $crTempFiles == "0" ]]; then
+		fd -I --glob "tempCodeRunner*.*" "${@:-.}" -x trash
+		files-code-runner-echo "$crTempFiles (CLEANED)"
+	fi
 }
