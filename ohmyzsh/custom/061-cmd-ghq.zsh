3efe7fb4399aa427f3df45171e4e5c7d1aa49f60
Philip Michael Raab <philip@cathedral.co.za>
2026-08-06 21:56:51 +0200

    omz custom - ghq - added update function

--- ohmyzsh/custom/061-cmd-ghq.zsh
+++ ohmyzsh/custom/061-cmd-ghq.zsh
@@ -1,18 +1,20 @@
 #########################################
-#		oh-my-zsh - custom: 005: aliases
-# version: 1                 2025 Aug 08
+#        oh-my-zsh - custom: 005: aliases
+# version: 1                  2025 Aug 08
 #############################-###########
 
 ## command
 # ghq
 #####################################################################
 if which ghq >/dev/null; then
+    # list ghq repositories and cd to selected one
 	function ghq-cd() {
 		local repo=$(ghq list | fzf)
 		[ -n "$repo" ] && cd "$(ghq root)/$repo"
 	}
 
-	function ghq-update() {
-		ghq list --full-path | xargs -I{} git -C {} pull --ff-only
-	}
+    # update ghq repositories    
+    function ghq-pull() {
+        for r in $(ghq list); do ghq get --shallow -u $r; done
+    }
 fi
