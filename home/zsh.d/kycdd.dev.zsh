c4f4a8ca391505dbb18ae08bb830965929959235
Philip Michael Raab <philip@cathedral.co.za>
2026-08-13 17:19:05 +0200

    fix: add forgit plugin to zsh configuration

--- home/zsh.d/kycdd.dev.zsh
+++ home/zsh.d/kycdd.dev.zsh
@@ -3,4 +3,4 @@
 ##################################################
 # export STARSHIP_CONFIG=~/.config/starship/develop.toml
 
-plugins+=($plugins autoupdate copybuffer eza zsh-autosuggestions zsh-interactive-cd zsh-navigation-tools zsh-you-should-use)
+plugins+=($plugins autoupdate copybuffer eza forgit zsh-autosuggestions zsh-interactive-cd zsh-navigation-tools zsh-you-should-use)
