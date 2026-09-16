3208fba2fd33039677c1a4ef9d531c1d4ddff5ce
Philip Michael Raab <philip@cathedral.co.za>
2026-09-16 14:12:13 +0200

    003a - plugin detect

--- home/zsh.d/kycdd-003a.zsh
+++ home/zsh.d/kycdd-003a.zsh
@@ -1,7 +1,7 @@
 ##################################################
 ## zsh: config
 ##################################################
-
+source "${0:A:h}/include/inc.plugins.detect.zsh"
 addToPathAndClean $HOME/.config/composer/vendor/bin "before"
 
 plugins+=($plugins autoupdate forgit zsh-interactive-cd zsh-navigation-tools zsh-autosuggestions)
