c360b875780bd727a6d3e2ea923fac06e63c9af9
Philip Michael Raab <philip@cathedral.co.za>
2026-09-19 18:46:30 +0200

    colours

--- home/zsh.d/kycdd-003a.zsh
+++ home/zsh.d/kycdd-003a.zsh
@@ -5,4 +5,3 @@ source "${0:A:h}/include/inc.plugins.detect.zsh"
 addToPathAndClean $HOME/.config/composer/vendor/bin "before"
 
 plugins+=($plugins autoupdate forgit zsh-interactive-cd zsh-navigation-tools zsh-autosuggestions)
-
